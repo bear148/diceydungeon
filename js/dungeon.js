a3f9fb169caba1d0e210d36bf6b9a625287491c8
bear148 <bizzeebear@hotmail.com>
2025-05-19 22:40:03 -0700

    Update base code

--- js/dungeon.js
+++ js/dungeon.js
@@ -1,3 +1,18 @@
-let muddy_swamp = {
-    id: 1,
+let dungeons = {
+    muddy_swamp: {
+        name: "Muddy Swamp",
+    },
+    haunted_crypt: {
+        name: "Haunted Crypt",
+    },
+    fiery_cave: {
+        name: "Fiery Cave",
+    },
+    forsaken_island: {
+        name: "Forsaken Island",
+    },
+    temple_of_gods: {
+        name: "Temple of Gods",
+    }
+
 }
